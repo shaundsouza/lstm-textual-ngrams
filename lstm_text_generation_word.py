96582a604b81f1f1a530977b54630820ff640e57
Shaun D'Souza <dsouza.shaun@gmail.com>
2019-04-11 20:19:49 +0530

    updated word

--- lstm_text_generation_word.py
+++ lstm_text_generation_word.py
@@ -17,6 +17,7 @@ from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
+from keras.models import save_model, load_model
 import numpy as np
 import random
 import sys
@@ -25,7 +26,7 @@ import io
 # path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
 path = "..\\..\\\\simple-examples\\data\\ptb.train.eos.txt"
 with io.open(path, encoding='utf-8') as f:
-    text = f.read().lower()[:10000]
+    text = open(path).read().lower().split()[:10000]
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -41,6 +42,9 @@ next_chars = []
 for i in range(0, len(text) - maxlen, step):
     sentences.append(text[i: i + maxlen])
     next_chars.append(text[i + maxlen])
+
+print(sentences[:10])
+print(next_chars[:10])
 print('nb sequences:', len(sentences))
 
 print('Vectorization...')
@@ -51,20 +55,23 @@ for i, sentence in enumerate(sentences):
         x[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
+# build the model: a single LSTM
 print("x ", x.shape)
 print("y ", y.shape)
 print(maxlen, len(chars))
 
-# build the model: a single LSTM
-print('Build model...')
-model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
-model.add(Dense(len(chars)))
-model.add(Activation('softmax'))
+train = True
+if train:
+    print('Build model...')
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+    model.add(Dense(len(chars)))
+    model.add(Activation('softmax'))
 
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
+    optimizer = RMSprop(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+else:
+    model = load_model('model.lstm')
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -75,45 +82,51 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+def on_epoch_end(epoch, logs=None):
+    if not epoch % 10 == 9:
+        return
 
-def on_epoch_end(epoch, logs):	
-	if not epoch % 10 == 9:
-		return
+    # Function invoked at end of each epoch. Prints generated text.
+    print()
+    print('----- Generating text after Epoch: %d' % epoch)
 
-	# Function invoked at end of each epoch. Prints generated text.
-	print()
-	print('----- Generating text after Epoch: %d' % epoch)
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    for diversity in [1.0]:
+        print('----- diversity:', diversity)
 
+        generated = []
+        sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + " ".join(sentence) + '"')
+        sys.stdout.write(" ".join(generated))
 
-	start_index = random.randint(0, len(text) - maxlen - 1)
-	for diversity in [0.2, 0.5, 1.0, 1.2]:
-		print('----- diversity:', diversity)
+        for i in range(400):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.
 
-		generated = ''
-		sentence = text[start_index: start_index + maxlen]
-		generated += sentence
-		print('----- Generating with seed: "' + sentence + '"')
-		sys.stdout.write(generated)
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
 
-		for i in range(400):
-			x_pred = np.zeros((1, maxlen, len(chars)))
-			for t, char in enumerate(sentence):
-				x_pred[0, t, char_indices[char]] = 1.
+            generated += [next_char]
+            sentence = sentence[1:] + [next_char]
 
-			preds = model.predict(x_pred, verbose=0)[0]
-			next_index = sample(preds, diversity)
-			next_char = indices_char[next_index]
-
-			generated += next_char
-			sentence = sentence[1:] + next_char
-
-			sys.stdout.write(next_char)
-			sys.stdout.flush()
-		print()
+            sys.stdout.write(" " + next_char)
+            sys.stdout.flush()
+        print()
 
 print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
-model.fit(x, y,
-          batch_size=128,
-          epochs=60,
-		  callbacks=[print_callback])
+if train:
+    model.fit(x, y,
+              batch_size=128,
+              epochs=60,
+              callbacks=[print_callback])
+
+    model.save('model.lstm')
+else:
+    for epoch in range(60):
+        on_epoch_end(epoch)
+
+
